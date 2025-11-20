7104992676f5d958214246cd8ca99e17e5c50d07
sudharsan2001 <sudharsangurukrishnan10@gmail.com>
2025-11-21 00:17:02 +0530

    get patients service completed

--- patient-service/src/main/java/com/pm/patient_service/service/PatientService.java
+++ patient-service/src/main/java/com/pm/patient_service/service/PatientService.java
@@ -1,7 +1,12 @@
 package com.pm.patient_service.service;
 
+import com.pm.patient_service.dto.PatientResponseDTO;
+import com.pm.patient_service.mapper.PatientMapper;
+import com.pm.patient_service.model.Patient;
 import com.pm.patient_service.repository.PatientRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class PatientService {
@@ -10,5 +15,16 @@ public class PatientService {
     public PatientService(PatientRepository patientRepository){
         this.patientRepository = patientRepository;
     }
+
+    public List<PatientResponseDTO> getPatients(){
+        List<Patient> patients = patientRepository.findAll();
+
+        List<PatientResponseDTO> patientResponseDTOs =
+                patients.stream().map(PatientMapper::toDTO).toList();
+
+        return patientResponseDTOs;
+    }
+
+
 
 }
